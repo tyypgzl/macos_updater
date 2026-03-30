8ac498eda5bb27100efe3756c69b86360c8a4773
Tayyip GUZEL <tguzeldev@gmail.com>
2026-03-30 09:27:22 +0300

    test(09-02): add failing tests for UpdateSource.getUpdateDetails() and getCurrentVersion() -> String
    - Replace MockUpdateSource.getLatestUpdateInfo with getUpdateDetails() returning UpdateDetails?
    - Replace ThrowingUpdateSource similarly
    - Add getCurrentVersion() String test to method channel tests
    - Fix update_check_result_test helper to not use removed buildNumber field

--- test/macos_updater_method_channel_test.dart
+++ test/macos_updater_method_channel_test.dart
@@ -1,27 +1,42 @@
-import "package:macos_updater/macos_updater_method_channel.dart";
-import "package:flutter/services.dart";
-import "package:flutter_test/flutter_test.dart";
+import 'package:macos_updater/macos_updater_method_channel.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   final platform = MethodChannelMacosUpdater();
-  const channel = MethodChannel("macos_updater");
+  const channel = MethodChannel('macos_updater');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
-        return "42";
+        switch (methodCall.method) {
+          case 'getPlatformVersion':
+            return '42';
+          case 'getCurrentVersion':
+            return '1.2.3';
+          default:
+            return null;
+        }
       },
     );
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
-  test("getPlatformVersion", () async {
-    expect(await platform.getPlatformVersion(), "42");
+  test('getPlatformVersion returns the mocked string', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+
+  test('getCurrentVersion returns a String semver value', () async {
+    final version = await platform.getCurrentVersion();
+    expect(version, isA<String>());
+    expect(version, '1.2.3');
   });
 }
